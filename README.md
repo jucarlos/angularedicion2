d9e88d1311203b4a8434cc70bb3308c8bc39771e
Carlos <jcarlosjccm@gmail.com>
2021-03-08 13:48:59 +0100

    Terminadas las directivas

--- README.md
+++ README.md
@@ -6,7 +6,20 @@
 ## Comandos de git.
 
 
-git status
+git status ( para ver como están los archivos)
 
 git add .  ( añadir al escenario de git )
+
+git status ( debería verse todo verde)
+
+git commit -m "Terminadas las rutas de la aplicación" ( para sacar la foto )
+
+
+Para recuperar del repositorio local algo borrado.
+
+git checkout -- .
+
+
+
+
 
--- src/app/pages/directivas/directivas.component.ts
+++ src/app/pages/directivas/directivas.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectivasComponent implements OnInit {
 
+  titulo = 'Centros educativos';
+
+  colegio = 'Colegio Infantes';
+
+  mostrarColegio = true;
+
+  datos = [1,2,4,5,6];
+
+  colegios = ['Nara', 'Infantes', 'Mayol', 'Sefarad'];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  mostrarOcultar(): void {
+
+    this.mostrarColegio = !this.mostrarColegio;
   }
 
 }
